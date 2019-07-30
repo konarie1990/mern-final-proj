789175b554aa23cdf8efcb6622704ed06f864b24
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-07-29 21:03:21 -0500

    created some components added some basic style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import website from "./website.png";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>Mern Stuff</h2>
-      </div>
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/" target="_blank">
+              <img src={website} width="30" height="30" alt="" />
+            </a>
+            <Link to="/" className="navbar-brand">
+              MERN-Stack Todo App
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
